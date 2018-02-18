8ba41fc6244cf580adf6e9bd121aefe0746cc20d
Kayvon Seif-Naraghi <kss2153@columbia.edu>
2018-02-17 22:41:57 -0500

    storyboard thing

--- tracksapp/tracks/ViewController.swift
+++ tracksapp/tracks/ViewController.swift
@@ -10,15 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var username_txt: UITextField!
+    
+    @IBOutlet weak var password_txt: UITextField!
+    
+    @IBAction func log_in_button(_ sender: UIButton) {
+    }
+    
+    @IBAction func make_account_button(_ sender: UIButton) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
 
 
 }
