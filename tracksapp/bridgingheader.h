6f73fba8798516e8a9a625053fcf71905d1736d7
Jill Shah <jillshah@Jills-MacBook-Pro.local>
2018-02-17 22:23:10 -0500

    KVVVVVVVV

---
+++ tracksapp/bridgingheader.h
@@ -0,0 +1,16 @@
+//
+//  bridgingheader.h
+//  tracks
+//
+//  Created by Jill Shah on 2/17/18.
+//  Copyright © 2018 Jill Shah. All rights reserved.
+//
+
+#ifndef bridgingheader_h
+#define bridgingheader_h
+
+#import <Google-Maps-iOS-Utils/GMUMarkerClustering.h>
+#import <Google-Maps-iOS-Utils/GMUGeometryRenderer.h>
+#import <Google-Maps-iOS-Utils/GMUGeoJSONParser.h>
+
+#endif /* bridgingheader_h */
